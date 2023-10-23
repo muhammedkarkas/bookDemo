581c236cfcb12b656d477b83ebac6dce81010e6b
muhammed karkaş <muhammed.karkas@outlook.com>
2023-10-23 18:22:48 +0300

    .

--- bookDemo/Controllers/BooksController.cs
+++ bookDemo/Controllers/BooksController.cs
@@ -1,12 +1,50 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using bookDemo.Data;
+using bookDemo.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
 
 namespace bookDemo.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/books")]
     [ApiController]
     public class BooksController : ControllerBase
     {
+        [HttpGet]
+        public IActionResult GetAllBooks()
+        {
+            var books = ApplicationContext.Books.ToList();
+            return Ok(books); //HttpStatus 200
+        }
+
+        [HttpGet("{id:int}")]
+        public IActionResult GetOneBook([FromRoute(Name ="id")]int id)
+        {
+            var book = ApplicationContext.Books.SingleOrDefault(x => x.Id.Equals(id));
+
+            if(book is null)
+                return NotFound(); //404
+            
+            return Ok(book  ); //HttpStatus 200
+        }
+        //HttpGet isteklerinde Action adının önemi yoktur.Route bilgisi olarak attribute içerisinde tanımlama yapılması gerekmektedir.
+
+        [HttpPost]
+        public IActionResult CreateOneBook([FromBody]Book book)
+        {
+            try
+            {
+                if (book is null)
+                    return BadRequest(); //400
+
+                ApplicationContext.Books.Add(book);
+                return StatusCode(201,book); //Created
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }   
 
     }
 }
---
+++ bookDemo/Data/ApplicationContext.cs
@@ -0,0 +1,19 @@
+﻿using bookDemo.Models;
+
+namespace bookDemo.Data
+{
+    public static class ApplicationContext
+    {
+        public static List<Book> Books { get; set; }    
+        static ApplicationContext()
+        {
+            //InMemory dataset
+            Books = new List<Book>()
+            {
+                new Book(){Id=1, Title = "İnce Memed", Price = 75},
+                new Book(){Id=2, Title = "İnce Memed 2", Price = 100},
+                new Book(){Id=3, Title = "İnce Memed 3", Price = 125}
+            };
+        }
+    }
+}
